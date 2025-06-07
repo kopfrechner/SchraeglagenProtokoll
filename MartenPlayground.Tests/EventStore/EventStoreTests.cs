7b68cece0a8d8eb73695498b83fc1ab289562330
Christoph Kopf <ckopf86@gmail.com>
2025-06-08 00:04:21 +0200

    add tests for rebuilding and projecting stream aggregates with snapshots; enhance assertions in EventStore integration tests

--- MartenPlayground.Tests/EventStore/EventStoreTests.cs
+++ MartenPlayground.Tests/EventStore/EventStoreTests.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 using Marten;
+using Marten.Events.Projections;
 using MartenPlayground.Tests.Setup;
 using Shouldly;
 using Weasel.Core;
@@ -50,16 +51,77 @@ public class EventStoreTests(PostgresContainerFixture fixture) : DocumentStoreTe
     [DependsOn(nameof(O1_create_eventstore_then_create_stream))]
     public async Task O2_load_bankaccount_aggregate()
     {
+        // Arrange
         var bankAccountId = GetFromBag<Guid>(
             nameof(O1_create_eventstore_then_create_stream),
             "BankAccountId"
         );
 
+        // Act
         await using var session = Session(EventStoreScenario01);
         var bankAccount = await session.Events.AggregateStreamAsync<BankAccount>(bankAccountId);
 
+        // Assert
         bankAccount
             .ShouldNotBeNull()
             .ShouldBe(new BankAccount(bankAccountId, "John Smith", Money.From(50, Currency.USD)));
     }
+
+    [Test]
+    [DependsOn(nameof(O1_create_eventstore_then_create_stream))]
+    public async Task O3_when_create_self_aggregate_projection_then_projection_should_null()
+    {
+        // Arrange
+        var bankAccountId = GetFromBag<Guid>(
+            nameof(O1_create_eventstore_then_create_stream),
+            "BankAccountId"
+        );
+
+        // Act
+        await using var session = Session(
+            EventStoreScenario01,
+            options => options.Projections.Snapshot<BankAccount>(SnapshotLifecycle.Inline)
+        );
+        await session.SaveChangesAsync();
+
+        // Assert
+        var bankAccount = await session.LoadAsync<BankAccount>(bankAccountId);
+        bankAccount.ShouldBeNull();
+    }
+
+    [Test]
+    [DependsOn(nameof(O3_when_create_self_aggregate_projection_then_projection_should_null))]
+    public async Task O4_rebuild_stream_then_projection_should_be_current_state()
+    {
+        // Arrange
+        var bankAccountId = GetFromBag<Guid>(
+            nameof(O1_create_eventstore_then_create_stream),
+            "BankAccountId"
+        );
+
+        // Act
+        await using var session = Session(
+            EventStoreScenario01,
+            options => options.Projections.Snapshot<BankAccount>(SnapshotLifecycle.Inline)
+        );
+
+        // (Re)build the self-aggregate projection
+        await session.DocumentStore.Advanced.RebuildSingleStreamAsync<BankAccount>(bankAccountId);
+
+        // Assert
+        var bankAccount = await session.LoadAsync<BankAccount>(bankAccountId);
+        bankAccount
+            .ShouldNotBeNull()
+            .ShouldBe(new BankAccount(bankAccountId, "John Smith", Money.From(50, Currency.USD)));
+    }
+
+    // Append events starting at self-aggregate
+
+    // Build a single-stream projection
+
+    // Build a multi-stream projection
+
+    // Archive a stream
+
+    // Build a notification handler
 }
